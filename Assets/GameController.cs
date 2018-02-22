8ee4eb2b0a96edc68a98c38efda54be679e4c972
davidwfranco <davidwfranco@gmail.com>
2018-02-22 14:26:22 -0300

    Initial Commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public static GameController instance;
+	public Camera cam;
+    public Vector2 upperCorner;
+    public Vector2 targeCamtWidth;
+    public float maxWidth;
+    public float maxHeigth;
+    public bool gameOver;
+    public GameObject gameLooseText;
+    public GameObject gameWinText;
+    private int score;
+    public Text scoreText;
+    private int lives;
+    public Text livesText;
+
+	// Awake is called when the script instance is being loaded.
+	void Awake()
+	{
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        
+        upperCorner = new Vector2(Screen.width, Screen.height);
+        targeCamtWidth = GameController.instance.cam.ScreenToWorldPoint(upperCorner);
+        maxWidth = targeCamtWidth.x;
+        maxHeigth = targeCamtWidth.y;
+        
+		lives = 3;
+
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rdb2d;
+	private Vector2 rawPosition;
+	private Vector2 targetPotision;
+	private float targetWidth;
+	private GameController gControl;
+	public float moveSpeed;
+	private float moveHDir;
+	private bool onAir;
+
+	// Use this for initialization
+	void Start () {
+		rdb2d = GetComponent<Rigidbody2D>();
+		gControl = GameController.instance;
+		onAir = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!gControl.gameOver)
+		{
+			moveHDir = Input.GetAxisRaw("Horizontal");
+			rdb2d.velocity = new Vector2((moveSpeed * moveHDir), 0);
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				rdb2d.AddForce(new Vector2(0.0f, 2.0f));
+				onAir = true;
+			} else {
+				onAir = false;
+			}
+		}	
+	}
+}
